a8f0bbdbb244554a60fbcb3d1dec522a1e9b7a7d
clarenous <gaoceyu@gmail.com>
2020-04-06 17:22:02 +0800

    new ot pattern

--- ot/ot.go
+++ ot/ot.go
@@ -7,112 +7,92 @@ import (
 	"github.com/clarenous/proxyot/curve"
 )
 
-// Params is the OT handler contains some key parameters.
-type Params struct {
-	GeneratorH curve.Point
-}
+// Params is the OT handler contains some parameters.
+type Params struct{}
 
-// New creates an OT handler with generator H.
-func New(H curve.Point) *Params {
-	return &Params{
-		GeneratorH: H,
-	}
+// New creates an OT handler.
+func New() *Params {
+	return &Params{}
 }
 
 // SealChoice refers to the process that receivers indicate the index of secret.
 // The input argument beta is the index.
-func SealChoice(beta *big.Int) (yp curve.Point, r *big.Int, err error) {
-	return defaultParams.SealChoice(beta)
+func SealChoice(beta *big.Int, pkA, pkB curve.Point) (Y curve.Point, L curve.Point, err error) {
+	return defaultParams.SealChoice(beta, pkA, pkB)
 }
 
 // SealChoice refers to the process that receivers indicate the index of secret.
 // The input argument beta is the index.
-func (params *Params) SealChoice(beta *big.Int) (yp curve.Point, r *big.Int, err error) {
-	// random number r
-	r, err = curve.RandomFieldElement(rand.Reader)
+func (params *Params) SealChoice(beta *big.Int, pkA, pkB curve.Point) (Y curve.Point, L curve.Point, err error) {
+	// random number l
+	var l *big.Int
+	l, err = curve.RandomFieldElement(rand.Reader)
 	if err != nil {
 		return
 	}
-	// calculate R = g^r
-	R := newPoint().ScalarBaseMult(r)
-	// calculate B = h^beta
-	B := newPoint().ScalarMult(params.GeneratorH, beta)
-	// calculate yp = g^r * h^beta = R + B
-	yp = newPoint().Add(R, B)
+	// calculate L = l * G
+	L = newPoint().ScalarBaseMult(l)
+	// calculate Y = beta * pkA + l * pkB
+	Y = newPoint().ScalarMult(pkA, beta)
+	Y.Add(Y, newPoint().ScalarMult(pkB, l))
 	return
 }
 
 // CalculateKeyPoints refers to the process that sender creates some sealed key points
-// given by the sender calculated yp.
-func CalculateKeyPoints(yp curve.Point, count int64) (kps []curve.Point, t *big.Int, tg curve.Point, err error) {
-	return defaultParams.CalculateKeyPoints(yp, count)
+// given by the sender calculated Y point.
+func CalculateKeyPoints(Y, L, pkA curve.Point, count int64) (kps []curve.Point, LPrime curve.Point, err error) {
+	return defaultParams.CalculateKeyPoints(Y, L, pkA, count)
 }
 
 // CalculateKeyPoints refers to the process that sender creates some sealed key points
-// given by the sender calculated yp.
-func (params *Params) CalculateKeyPoints(yp curve.Point, count int64) (kps []curve.Point, t *big.Int, T curve.Point, err error) {
+// given by the sender calculated Y point.
+func (params *Params) CalculateKeyPoints(Y, L, pkA curve.Point, count int64) (kps []curve.Point, LPrime curve.Point, err error) {
 	// random number t
+	var t *big.Int
 	t, err = curve.RandomFieldElement(rand.Reader)
 	if err != nil {
 		return
 	}
-	// calculate T = g^t
-	T = newPoint().ScalarBaseMult(t)
 	// calculate puzzles
 	kps = make([]curve.Point, count)
 	for i := int64(1); i <= count; i++ {
-		kps[i-1] = params.calculateKeyPoint(t, yp, big.NewInt(i))
+		kps[i-1] = params.calculateKeyPoint(t, Y, L, pkA, big.NewInt(i))
 	}
+	// calculate LPrime = t * L
+	LPrime = newPoint().ScalarMult(L, t)
 	return
 }
 
-func (params *Params) calculateKeyPoint(t *big.Int, yp curve.Point, ordinal *big.Int) curve.Point {
-	// h^-i
-	point := newPoint().ScalarMult(params.GeneratorH, big.NewInt(0).Sub(curve.Order, ordinal))
-	// y * h^-i
-	point = point.Add(yp, point)
-	// (y * h^-i) ^ t
-	return point.ScalarMult(point, t)
+func (params *Params) calculateKeyPoint(t *big.Int, yp, lp, pkA curve.Point, ordinal *big.Int) (kpi curve.Point) {
+	// kpi = t * yp - i * t * pkA
+	kpi = newPoint().ScalarMult(yp, t)
+	temp := newPoint().ScalarMult(pkA, new(big.Int).Mul(ordinal, t))
+	kpi.Add(kpi, temp.Neg(temp))
+	return
 }
 
 // RevealKeyPoint refers to the process that receiver reveals real key point
 // given by the sender calculated sealed key point.
-func RevealKeyPoint(T curve.Point, r *big.Int) curve.Point {
-	return defaultParams.RevealKeyPoint(T, r)
+func RevealKeyPoint(LPrime curve.Point, skB *big.Int) curve.Point {
+	return defaultParams.RevealKeyPoint(LPrime, skB)
 }
 
 // RevealKeyPoint refers to the process that receiver reveals real key point
 // given by the sender calculated sealed key point.
-func (params *Params) RevealKeyPoint(T curve.Point, r *big.Int) curve.Point {
-	return newPoint().ScalarMult(T, r)
+func (params *Params) RevealKeyPoint(LPrime curve.Point, skB *big.Int) curve.Point {
+	return newPoint().ScalarMult(LPrime, skB)
 }
 
 func newPoint() curve.Point {
-	return curve.NewPoint(curve.TypeG2)
-}
-
-var defaultGeneratorH curve.Point
-
-func initDefaultGeneratorH() error {
-	h, err := curve.RandomFieldElement(rand.Reader)
-	if err != nil {
-		return err
-	}
-	defaultGeneratorH = newPoint().ScalarBaseMult(h)
-	return nil
+	return curve.NewPoint(curve.TypeG1)
 }
 
 var defaultParams *Params
 
 func initDefaultParams() {
-	defaultParams = &Params{
-		GeneratorH: defaultGeneratorH,
-	}
+	defaultParams = &Params{}
 }
 
 func init() {
-	if err := initDefaultGeneratorH(); err != nil {
-		panic(err)
-	}
 	initDefaultParams()
 }
--- ot/ot_test.go
+++ ot/ot_test.go
@@ -1,9 +1,11 @@
 package ot_test
 
 import (
+	"crypto/rand"
 	"math/big"
 	"testing"
 
+	"github.com/clarenous/proxyot/curve"
 	"github.com/clarenous/proxyot/ot"
 )
 
@@ -12,18 +14,28 @@ func TestOT(t *testing.T) {
 	var betaI int64 = 23
 	var beta = big.NewInt(betaI)
 
-	yp, r, err := ot.SealChoice(beta)
+	_, pkA, err := curve.NewRandomPoint(curve.TypeG1, rand.Reader)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	kps, _, tg, err := ot.CalculateKeyPoints(yp, count)
+	skB, pkB, err := curve.NewRandomPoint(curve.TypeG1, rand.Reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	Y, L, err := ot.SealChoice(beta, pkA, pkB)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	kps, LPrime, err := ot.CalculateKeyPoints(Y, L, pkA, count)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	kpA := kps[betaI-1]
-	kpB := ot.RevealKeyPoint(tg, r)
+	kpB := ot.RevealKeyPoint(LPrime, skB)
 	if kpA.String() != kpB.String() {
 		t.Errorf("Key Point not equal:\nAlice: %s\nBob: %s", kpA.String(), kpB.String())
 	}
